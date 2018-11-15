90fbb60724e1d13b83c86fdb05e855affbfaea65
Unknown <przemek.rakowski@gmail.com>
2018-11-15 18:52:53 +0100

    webpack config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import { apolloProvider } from "./graphQlConfig";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
